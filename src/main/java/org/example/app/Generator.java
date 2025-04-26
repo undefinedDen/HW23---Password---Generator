0398d79a898e1c0cf92d4e0cff75dc1c424a2f4c
undefinedDen <tunevdenis123@gmail.com>
2025-04-26 06:48:50 +0300

    update Generatot - Library

--- src/main/java/org/example/app/Generator.java
+++ src/main/java/org/example/app/Generator.java
@@ -12,17 +12,27 @@ public class Generator {
         AppValidator validate = new AppValidator();
         Random random = new Random();
         StringBuilder password = new StringBuilder();
+        boolean flag = false;
         int length = 0;
         try {
             length = Integer.parseInt(inputLength);
             validate.validatePasswordLength(length);
+            flag = true;
         } catch (AppException | NumberFormatException e) {
             System.out.print(e.getMessage());
+
         }
 
         for (int i = 0; i < length; i++) {
             password.append(random.nextInt(0, 9));
         }
-        return password.toString();
+        if(flag){
+            return "Your password was generate: " + password;
+        }else{
+            return"Your password was not generate " +
+                    "because your length that you input contains of letters or other symbols" +
+                    "or numbers that less than 1";
+        }
+
     }
 }
